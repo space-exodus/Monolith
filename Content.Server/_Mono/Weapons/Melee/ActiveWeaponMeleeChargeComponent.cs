2319205530137d1452ea107d49157851d4bf2b27
NazrinNya <137837419+NazrinNya@users.noreply.github.com>
2025-11-18 00:02:52 +0300

    Charged Melee Attacks (#2647)
    Co-authored-by: NazrinNya <macrohard@macrohard.localhost>
    Co-authored-by: monolith8319 <195513600+monolith8319@users.noreply.github.com>

---
+++ Content.Server/_Mono/Weapons/Melee/ActiveWeaponMeleeChargeComponent.cs
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2025 NazrinNya
+//
+// SPDX-License-Identifier: MPL-2.0
+
+namespace Content.Server._Mono.Weapons.Melee;
+
+/// <summary>
+/// Used to mark if melee weapon is in active charged state.
+/// </summary>
+[RegisterComponent]
+public sealed partial class ActiveWeaponMeleeChargeComponent : Component;
